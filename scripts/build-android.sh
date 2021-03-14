53624ca66ec49b23b13dbd28c6ea3d798bb6972b
Logan Keenan <d.logankeenan@gmail.com>
2021-03-14 06:46:36 -0500

    remove cp script from android release

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -28,7 +28,6 @@ if [ -n "$1" ] && [ $1 = '--release' ]
 then
   cp rust-android/target/aarch64-linux-android/release/librust_android.so  ${LIBS_DIR}/arm64-v8a/librust_android.so
   cp rust-android/target/armv7-linux-androideabi/release/librust_android.so  ${LIBS_DIR}/armeabi-v7a/librust_android.so
-  cp rust-android/target/i686-linux-android/release/librust_android.so  ${LIBS_DIR}/x86/librust_android.so
 else
   cp rust-android/target/aarch64-linux-android/debug/librust_android.so  ${LIBS_DIR}/arm64-v8a/librust_android.so
   cp rust-android/target/armv7-linux-androideabi/debug/librust_android.so  ${LIBS_DIR}/armeabi-v7a/librust_android.so
